1e511043471748d7d1b0fd6c555cf1388e64e5ec
Quentin Lefèvre <qlefevre@gmail.com>
2023-01-31 14:18:52 +0100

    Adds logs and PORT environment variable

--- java-container/src/main/java/Handler.java
+++ java-container/src/main/java/Handler.java
@@ -1,19 +1,24 @@
 import io.undertow.Undertow;
 import io.undertow.util.Headers;
+import org.apache.commons.lang3.SystemUtils;
+
+import static io.undertow.UndertowLogger.ROOT_LOGGER;
+
 public class Handler {
 
-    public static final int DEFAULT_PORT = 8080;
+    public static final String DEFAULT_PORT = "8080";
 
     public static void main(String[] args) {
-        int port = Integer.getInteger("PORT",DEFAULT_PORT);
+        String port = SystemUtils.getEnvironmentVariable("PORT", DEFAULT_PORT);
+        ROOT_LOGGER.infof("HTTP server listening on 0.0.0.0:" + port);
         Undertow server = Undertow.builder()
-                .addHttpListener(port, "0.0.0.0")
+                .addHttpListener(Integer.parseInt(port), "0.0.0.0")
                 .setHandler(exchange -> {
                     exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
                     exchange.getResponseSender().send("""
-                        {
-                            "message":  "Hello, World from Scaleway Container !"
-                        }""");
+                            {
+                                "message":  "Hello, World from Scaleway Container !"
+                            }""");
                 }).build();
         server.start();
     }
